b64b97ea2e62a3c6eb8c6b35787a52c615b0ba97
Eddie Rangel <hacker@eddierangel.com>
2019-03-08 23:53:47 -0800

    Update GroupChannelActivity.java
    Removed explicit cast. Added Log TAG.

--- app/src/main/java/com/eddierangel/southkern/android/groupchannel/GroupChannelActivity.java
+++ app/src/main/java/com/eddierangel/southkern/android/groupchannel/GroupChannelActivity.java
@@ -15,12 +15,14 @@ import com.eddierangel.southkern.android.openchannel.OpenChannelListFragment;
 // TODO: Add Documentation to Public Interface
 public class GroupChannelActivity extends AppCompatActivity{
 
+    private static final String TAG = "GroupChannelActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_channel);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_group_channel);
+        Toolbar toolbar = findViewById(R.id.toolbar_group_channel);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
