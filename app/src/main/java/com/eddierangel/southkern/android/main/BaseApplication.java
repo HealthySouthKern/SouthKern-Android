9f1f35291fbc846d080fc5d5e0c62ad2b1d4905c
Eddie Rangel <hacker@eddierangel.com>
2019-03-08 23:52:38 -0800

    Update BaseApplication.java
    Removed stale code

--- app/src/main/java/com/eddierangel/southkern/android/main/BaseApplication.java
+++ app/src/main/java/com/eddierangel/southkern/android/main/BaseApplication.java
@@ -11,9 +11,7 @@ import io.fabric.sdk.android.Fabric;
 // TODO: Add Documentation to Public Interface
 public class BaseApplication extends Application {
 
-    // TODO: What else can be or should be defined here?
     private static final String APP_ID = "91EEEE6D-8D4A-4FA4-9A52-0B8E2DDD64D0"; // SouthKernTest01
-    public static final String VERSION = "1.0.0";
 
     @Override
     public void onCreate() {
